a23ba54f459f55ba6871e0a8f4e9dfee4656a900
Muna Chimoriya <muna.chimoriya.2022@student.lhschools.org>
2020-03-05 11:56:02 -0500

    OddnessMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,6 +3,20 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+		testOddness();
+	}
+	public static void testOddness() {
+     Scanner testOddness;
+     testOddness = new Scanner(System.in);
+
+     System.out.println("Give me a positive number");
+
+
+     int number = testOddness.nextInt();
+
+          System.out.println(number + " is odd: "  +(number % 2 !=0 ));
+
+
+
+	}
 }
