4a55fa0e9df38a73512d69a47674ba33ae0939a5
Muna Chimoriya <muna.chimoriya.2022@student.lhschools.org>
2020-03-06 12:06:06 -0500

    keepContainedMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,22 +1,38 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-		testOddness();
+	public static void main(String[] args) {
+		//testOddness();
+		keepContained();
+
 	}
+
 	public static void testOddness() {
      Scanner testOddness;
      testOddness = new Scanner(System.in);
 
      System.out.println("Give me a positive number");
-
-
      int number = testOddness.nextInt();
 
-          System.out.println(number + " is odd: "  +(number % 2 !=0 ));
+          System.out.println(number + " is odd: " + (number%2 != 0));
+
 
 
 
 	}
+    public static void keepContained(){
+	    int number;
+	    Scanner keyboard;
+	    keyboard = new Scanner(System.in);
+        System.out.println("Give me a positive number");
+        number = keyboard.nextInt();
+
+        System.out.println( " number contained is ");
+        System.out.println((number%8)+ 5);
+
+
+    }
 }
